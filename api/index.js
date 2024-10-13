04f846812d2a0849fa05297ab4394f4ad9f635d6
omniroot <omnirootofc@gmail.com>
2024-10-13 07:14:21 +0300

    init

---
+++ api/index.js
@@ -0,0 +1,19 @@
+// require('dotenv').config();
+
+import express from 'express';
+
+const app = express();
+
+app.get('/', async (req, res) => {
+  try {
+    const data = await fetch("https://www.anilibria.tv/release/make-heroine-ga-oosugiru.html")
+    res.send(data);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
